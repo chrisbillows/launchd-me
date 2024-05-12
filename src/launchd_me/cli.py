0af4eb2b0b529e36e41a22ec51f0b4d903fb9558
chrisbillows <christopherbillows@gmail.com>
2024-05-12 14:23:28 +0100

    Add argparse `create` and `list` skeleton.

--- src/launchd_me/cli.py
+++ src/launchd_me/cli.py
@@ -3,20 +3,38 @@ import argparse
 import launchd_me
 
 def get_args():
+    """Get command line args.
+    
+    Maybe something like:
+        
+    `launchd-me create hello.py interval 300`
+    `launchd-me list`
+    `launchd-me install`
+    `launchd-me uninstall`
+    `launchd-me show`
+    """
     parser = argparse.ArgumentParser(
-        prog="Launchd-me",
+        prog="ldm",
         description="Create and manage plist files."
         )
-    parser.add_argument("Script")
-    parser.add_argument("ScheduleType")
-    parser.add_argument("Schedule")
+    subparsers = parser.add_subparsers(required=True)
+    
+    parser_create = subparsers.add_parser("create", help="Create a plist file from a given script.")
+    parser_create.add_argument("script_path", help="path to the script to automate.")
+    parser_create.add_argument("schedule_type", choices=["interval", "calendar"], help="schedule_type. 'interval' schedules are defined seconds. 'calendar' schedules are defined usings times, days, weeks etc. See Documentation for more details.")
+    parser_create.add_argument("schedule_details", help="How often the script is run. E.g. `300` for 'interval'. ")
+    parser_create.add_argument("-a", "--auto-install", default=True, help="Automatically load the plist file once created. Defaults to True.")
+    parser_create.add_argument("-m", "--make-executable", default=True, help="Check if the specified script is already an executable and, if it is not, make it exectuable. Defaults to True.")
+
+    parser_list = subparsers.add_parser("list", help="List all tracked plist files.")
+    parser_list.add_argument("plist_id", help="Display details of given plist file.")      
+        
     args = parser.parse_args()
     return args
 
 
 def main():
-    print("Main is running")
-    
+    args = get_args()
     # plc = launchd_me.PlistCreator("hello.py", launchd_me.ScheduleType.interval, 300)
     # my_list = [
     #     plc.script_name, 
