0bb9a4766876a781ee8c5f31591f3315c06228e1
chrisbillows <christopherbillows@gmail.com>
2024-07-21 17:36:30 +0100

    #45: Make `get_args` a class.

--- src/launchd_me/cli.py
+++ src/launchd_me/cli.py
@@ -15,9 +15,64 @@ from launchd_me.plist import (
 )
 
 USER_CONFIG = UserConfig()
+LOGO = """
+ ___      _______  __   __  __    _  _______  __   __  ______     __   __  _______  __ 
+|   |    |   _   ||  | |  ||  |  | ||       ||  | |  ||      |   |  |_|  ||       ||  |
+|   |    |  |_|  ||  | |  ||   |_| ||       ||  |_|  ||  _    |  |       ||    ___||  |
+|   |    |       ||  |_|  ||       ||       ||       || | |   |  |       ||   |___ |  |
+|   |___ |       ||       ||  _    ||      _||       || |_|   |  |       ||    ___||__|
+|       ||   _   ||       || | |   ||     |_ |   _   ||       |  | ||_|| ||   |___  __ 
+|_______||__| |__||_______||_|  |__||_______||__| |__||______|   |_|   |_||_______||__|
+"""
+CLI_TEXT = {
+    "PARSER": {
+        "DESCRIPTION": "Easily schedule your scripts on macOS."
+        },
+    "SUBPARSER": {
+        "TITLE": "subcommands",
+        "DESCRIPTION": "",
+        },
+        "COMMANDS": {
+            "CREATE": {
+                "HELP": "Create a plist file to schedule a given script.",
+                "ARGS": {
+                    "SCRIPT_PATH_HELP": "path to the script to schedule.",
+                    "SCHEDULE_TYPE_HELP": """schedule_type. An 'interval' schedule requires seconds e.g. 300. A 'calendar'  
+                                             schedules require a dictionary of fields e.g. {'Weekday': 1, 'Hour': 8}.
+                                             Allowed fields are: 'Minute', 'Hour', 'Day', 'Weekday', 'Month'. See the 
+                                             documentation for more details.""",
+                    "SCHEDULE_DETAILS_HELP": "schedule for running the script e.g. 300 or {'Weekday': 1, 'Hour': 8}.",
+                    "DESCRIPTION_HELP": "description of what you're automating e.g. daily downloads tidy.",
+                    "MAKE_EXECUTABLE_HELP": "ensure the specified script is executable. defaults to true.",
+                    "AUTO_INSTALL_HELP": "load the plist/schedule the script automatically. defaults to true",
+                },
+            },
+            "LIST": {
+                "DESCRIPTION": "List all tracked plist files or show a given [plist_id]",
+                "HELP": "list all tracked plist files or show a given [plist_id]",
+                "ARGS": {"PLIST_ID_HELP": "display details of [plist_id]"},
+            },
+            "INSTALL": {
+                "DESCRIPTION": "Install a given plist file.",
+                "HELP": "install a given plist file",
+                "ARGS": {
+                    "PLIST_ID_HELP": "the plist file to install"
+                }
+            },
+            "UNINSTALL": {
+                "HELP": "uninstall a given plist file",
+                "ARGS": {
+                    "PLIST_ID_HELP": "the plist file to un-install"
+                        }
+            },
+            "RESET": {
+                "HELP": "delete the current db and plist directory. currently does not unload or delete existing plist symlinks"
+            }
+        },
+    }
 
 
-def valid_path(path_str):
+def valid_path(path_str: str) -> Path:
     """Check if the path is a valid file path and convert it to pathlib.Path."""
     path = Path(path_str).resolve()
     if not path.is_file():
@@ -25,84 +80,113 @@ def valid_path(path_str):
     return path
 
 
-def get_args():
-    """Get command line args."""
-    parser = argparse.ArgumentParser(
-        prog="ldm", description="Create and manage plist files."
-    )
-    subparsers = parser.add_subparsers(dest="command", required=True)
+class ParserCreator:
+    def __init__(self) -> None:
+        self.parser = argparse.ArgumentParser(
+            prog="ldm", description=CLI_TEXT["PARSER"]["DESCRIPTION"]
+        )
+        self.parser.set_defaults(func=self._default_action)
+        self.subparsers = self.parser.add_subparsers(
+            title=CLI_TEXT["SUBPARSER"]["TITLE"], 
+            description=CLI_TEXT["SUBPARSER"]["DESCRIPTION"],
+        )
 
-    parser_create = subparsers.add_parser(
-        "create", help="Create a plist file from a given script."
-    )
-    parser_create.add_argument(
-        "script_path", type=valid_path, help="path to the script to automate."
-    )
-    parser_create.add_argument(
-        "schedule_type",
-        choices=["interval", "calendar"],
-        help="""schedule_type. 'interval' schedules are defined seconds
-                . 'calendar' schedules are defined usings times, days,
-                weeks etc. See Documentation for more details.""",
-    )
-    parser_create.add_argument(
-        "schedule_details",
-        type=int,
-        help="How often the script is run. E.g. `300` for 'interval'. ",
-    )
-    parser_create.add_argument(
-        "description",
-        type=str,
-        help="A description of what you're automating. Enter x if you're just going to remember. Ha ha.",
-    )
-    parser_create.add_argument(
-        "-m",
-        "--make-executable",
-        default=True,
-        type=bool,
-        help="""Ensure the specified script is executable. Defaults to
-                True.""",
-    )
-    parser_create.add_argument(
-        "-a",
-        "--auto-install",
-        default=True,
-        type=bool,
-        help="Automatically load the plist file into OSX once created. Defaults to True.",
-    )
+    def create_parser(self) -> argparse.ArgumentParser:
+        self._add_create_command()
+        self._add_list_command()
+        self._add_install_command()
+        self._add_uninstall_command()
+        self._add_reset_command()
+        return self.parser
+    
+    def _default_action(self, args: argparse.Namespace) -> None:
+        print("ERROR: ldm requires a command. See the help below...\n")
+        self.parser.print_help()
 
-    parser_list = subparsers.add_parser("list", help="List all tracked plist files.")
-    parser_list.add_argument(
-        "-p", "--plist-id", type=int, help="Display details of given plist file."
-    )
+    def _add_create_command(self) -> None:
+        parser_create = self.subparsers.add_parser(
+            "create", 
+            help=CLI_TEXT["COMMANDS"]["CREATE"]["HELP"]
+            )
+        parser_create.set_defaults(func=create_plist)
+        parser_create.add_argument(
+            "script_path", 
+            type=valid_path, 
+            help=CLI_TEXT["COMMANDS"]["CREATE"]["ARGS"]["SCRIPT_PATH_HELP"]
+        )
+        parser_create.add_argument(
+            "schedule_type",
+            choices=["interval", "calendar"],
+            help=CLI_TEXT["COMMANDS"]["CREATE"]["ARGS"]["SCHEDULE_TYPE_HELP"]
+        )
+        parser_create.add_argument(
+            "schedule_details", 
+            type=int, 
+            help=CLI_TEXT["COMMANDS"]["CREATE"]["ARGS"]["SCHEDULE_DETAILS_HELP"]
+        )
+        parser_create.add_argument(
+            "description", 
+            type=str, 
+            help=CLI_TEXT["COMMANDS"]["CREATE"]["ARGS"]["DESCRIPTION_HELP"]
+        )
+        parser_create.add_argument(
+            "-m",
+            "--make-executable",
+            default=True,
+            type=bool,
+            help=CLI_TEXT["COMMANDS"]["CREATE"]["ARGS"]["MAKE_EXECUTABLE_HELP"]
+        )
+        parser_create.add_argument(
+            "-a", 
+            "--auto-install", 
+            default=True, 
+            type=bool, 
+            help=CLI_TEXT["COMMANDS"]["CREATE"]["ARGS"]["AUTO_INSTALL_HELP"]
+        )
 
-    parser_install = subparsers.add_parser(
-        "install", help="Install a given plist file."
-    )
-    parser_install.add_argument("plist_id", help="The plist id install.")
+    def _add_list_command(self) -> None:
+        parser_list = self.subparsers.add_parser(
+            "list", 
+            description=CLI_TEXT["COMMANDS"]["LIST"]["DESCRIPTION"], 
+            help=CLI_TEXT["COMMANDS"]["LIST"]["HELP"]
+        )
+        parser_list.set_defaults(func=list_plists)
+        parser_list.add_argument(
+            "plist_id", 
+            type=int, 
+            nargs="?", 
+            help=CLI_TEXT["COMMANDS"]["LIST"]["ARGS"]["PLIST_ID_HELP"]
+        )
 
-    parser_uninstall = subparsers.add_parser(
-        "uninstall", help="Uninstall a given plist file."
-    )
-    parser_uninstall.add_argument("plist_id", help="The plist to un-install.")
+    def _add_install_command(self) -> None:
+        parser_install = self.subparsers.add_parser(
+            "install", 
+            help=CLI_TEXT["COMMANDS"]["INSTALL"]["HELP"]
+        )
+        parser_install.set_defaults(func=install_plist)
+        parser_install.add_argument(
+            "plist_id", 
+            help="The plist id install")
 
-    parser_show = subparsers.add_parser("show", help="Show a given plist file.")
-    parser_show.add_argument("plist_id", help="The plist to display.")
+    def _add_uninstall_command(self) -> None:
+        parser_uninstall = self.subparsers.add_parser(
+            "uninstall", 
+            help=CLI_TEXT["COMMANDS"]["UNINSTALL"]["HELP"]
+        )
+        parser_uninstall.set_defaults(func=uninstall_plist)
+        parser_uninstall.add_argument(
+            "plist_id", 
+            help="The plist to un-install")
 
-    parser_reset = subparsers.add_parser(
-        "reset",
-        help="Delete the current db and plist directory. NOTE: Currently does not "
-        "unload or delete existing plist symlinks.",
-    )
-    args = parser.parse_args()
-    return args
+    def _add_reset_command(self) -> None:
+        parser_reset = self.subparsers.add_parser(
+            "reset",
+            help=CLI_TEXT["COMMANDS"]["RESET"]["HELP"]
+        )
+        parser_reset.set_defaults(func=reset_user)
 
 
-def create_plist(args):
-    # So PlistCreator generates the plist, does the install, does the execute, returns
-    # the final thing and updates the db.  It's simpler to have all that logic in one
-    # function to avoid having to PASS it around in partial state.
-    # i.e. we'd have the file generated but no id yet etc.
+def create_plist(args: argparse.Namespace) -> None:
     logger.debug("Instantiating PlistCreator.")
     plc = PlistCreator(
         args.script_path,
@@ -117,7 +201,7 @@ def create_plist(args):
     plc.driver()
 
 
-def list_plists(args):
+def list_plists(args: argparse.Namespace) -> None:
     db_getters = PlistDbGetters(USER_CONFIG)
     if args.plist_id:
         logger.debug("Displaying a single plist file detail.")
@@ -131,7 +215,7 @@ def list_plists(args):
         db_all_rows_displayer.display_all_tracked_plist_files_table(all_rows)
 
 
-def install_plist(args):
+def install_plist(args: argparse.Namespace) -> None:
     db_getter = PlistDbGetters(USER_CONFIG)
     db_setter = PlistDbSetters(USER_CONFIG)
     install_manager = PlistInstallationManager(USER_CONFIG, db_setter)
@@ -165,7 +249,7 @@ def uninstall_plist(args: argparse.Namespace) -> None:
     install_manager.uninstall_plist(args.plist_id, symlink_to_plist)
 
 
-def reset_user(args: argparse.Namespace):
+def reset_user(args: argparse.Namespace) -> None:
     logger.debug("Fetching the project directory")
     project_dir = USER_CONFIG.project_dir
     logger.debug(f"Project directory: {project_dir}")
@@ -178,19 +262,10 @@ def main():
     user_config = UserConfig()
     ldm = LaunchdMeInit(user_config)
     ldm.initialise_launchd_me()
-    args = get_args()
-    command_dispatcher = {
-        "create": create_plist,
-        "list": list_plists,
-        "install": install_plist,
-        "uninstall": uninstall_plist,
-        "reset": reset_user,
-    }
-    command = args.command
+    parser_creator = ParserCreator()
+    parser = parser_creator.create_parser()
+    args = parser.parse_args()
     try:
-        if command in command_dispatcher:
-            command_dispatcher[command](args)
-        else:
-            print(f"Unknown command: {command}")
+        args.func(args)
     except PlistFileIDNotFound as error:
-        print(error)
+            print(error)
