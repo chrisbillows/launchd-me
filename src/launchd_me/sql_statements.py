78e10fec428c01b9d0f1deb64971a78d499012a9
chrisbillows <96173696+chrisbillows@users.noreply.github.com>
2024-07-28 15:07:03 +0100

    #65: Move all SQL statements into `sql_statements.py`

--- src/launchd_me/sql_statements.py
+++ src/launchd_me/sql_statements.py
@@ -1,4 +1,16 @@
-CREATE_TABLE_PLIST_FILES = """
+CREATE_TABLE_INSTALLATION_EVENTS = """
+CREATE TABLE IF NOT EXISTS InstallationEvents (
+    EventID INTEGER PRIMARY KEY AUTOINCREMENT,
+    FileID INTEGER,
+    EventType TEXT NOT NULL CHECK (EventType IN ('install', 'uninstall')),
+    EventDate TEXT NOT NULL,
+    Success INTEGER NOT NULL CHECK (Success IN (0, 1)), -- 0 = False, 1 = True
+    FOREIGN KEY (FileID) REFERENCES PlistFiles (FileID)
+);
+"""
+
+
+CREATE_TABLE_PLISTFILES = """
 CREATE TABLE IF NOT EXISTS PlistFiles (
     PlistFileID INTEGER PRIMARY KEY AUTOINCREMENT,
     PlistFileName TEXT NOT NULL,
@@ -12,18 +24,7 @@ CREATE TABLE IF NOT EXISTS PlistFiles (
 );
 """
 
-CREATE_TABLE_INSTALLATION_EVENTS = """
-CREATE TABLE IF NOT EXISTS InstallationEvents (
-    EventID INTEGER PRIMARY KEY AUTOINCREMENT,
-    FileID INTEGER,
-    EventType TEXT NOT NULL CHECK (EventType IN ('install', 'uninstall')),
-    EventDate TEXT NOT NULL,
-    Success INTEGER NOT NULL CHECK (Success IN (0, 1)), -- 0 = False, 1 = True
-    FOREIGN KEY (FileID) REFERENCES PlistFiles (FileID)
-);
-"""
-
-PLISTFILES_TABLE_INSERT_INTO = """
+PLISTFILES_INSERT_RECORD_INTO = """
 INSERT INTO PlistFiles (
     PlistFileName,
     ScriptName,
@@ -37,11 +38,21 @@ INSERT INTO PlistFiles (
 VALUES (?, ?, ?, ?, ?, ?, ?, ?);
 """
 
-PLISTFILES_TABLE_SELECT_ALL = "SELECT * FROM PlistFiles"
-PLISTFILES_TABLE_SELECT_SINGLE_PLIST_FILE = (
-    "SELECT * FROM  PlistFiles WHERE plistFileId = ?"
-)
 
-PLISTFILES_TABLE_GET_INSTALL_STATUS = (
+PLISTFILES_COUNT_ALL_ROWS = "SELECT COUNT(*) FROM PlistFiles"
+PLISTFILES_SET_CURRENT_STATE_RUNNING = (
+    "UPDATE PlistFiles SET CurrentState = 'running' WHERE PlistFileID = ?"
+)
+PLISTFILES_SET_CURRENT_STATE_INACTIVE = (
+    "UPDATE PlistFiles SET CurrentState = 'inactive' WHERE PlistFileID = ?"
+)
+PLISTFILES_GET_INSTALL_STATUS = (
     "SELECT CurrentState FROM  PlistFiles WHERE plistFileId = ?"
+)
+PLISTFILES_SELECT_ALL = "SELECT * FROM PlistFiles"
+PLISTFILES_SELECT_SINGLE_PLIST_FILE = "SELECT * FROM  PlistFiles WHERE plistFileId = ?"
+PLISTFILES_SELECT_ALL_FIELDS_FOR_LIST_COMMAND = (
+    "SELECT PlistFileID, PlistFileName, ScriptName, CreatedDate, "
+    "ScheduleType, ScheduleValue, CurrentState FROM PlistFiles"
+    " ORDER BY PlistFileID"
 )
